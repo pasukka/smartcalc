06456da4a2b9be9d012fb5032ff8ed8bbd843f7d
SevenStreams <sevenstr@student.21-school.ru>
2024-02-01 23:47:31 +0700

    removed unnecessary comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,9 @@
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
-  
   Model model;
   Controller controller(&model);
   View view(&controller);
   view.startEventLoop();
   return a.exec();
-  
-  // return 1;
-
-  // QApplication a(argc, argv);
-  // MainWindow w;
-  // w.show();
-  // return a.exec();
 }
-
